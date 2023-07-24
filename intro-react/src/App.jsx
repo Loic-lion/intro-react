1af34d820dae72bb7b14e3782c677ace52009b14
Loic Lion <loic.lion.contact@gmail.com>
2023-07-24 14:17:28 +0200

    suite app jsx

--- intro-react/src/App.jsx
+++ intro-react/src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
-import React from "react";
 import ToDoList from "./ToDoList";
 
 function App() {
+  const [todos, setTodos] = useState([{ id: 1, name: "Learn React", complete: false}]);
+  const todoNameRef = useRef()
+  
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+  }
+  
   return (
     <>
-      <ToDoList />
-      <input type="text" placeholder="type a new todo" />
-      <button>Add Todo</button>
-      <button>Clear Completed Todos</button>
+      <input ref={todoNameRef} type="text" placeholder="type a new todo" />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button>Clear Completed</button>
       <hr></hr>
       <h2>Todos</h2>
       <div>0 left to do</div>
+      <ToDoList todos={todos} />
     </>
   );
 }
